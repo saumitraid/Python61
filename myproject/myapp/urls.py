adc37821a10250ea5489bd161e017f3e7ddced92
saumitraid <saumitraid.das@gmail.com>
2026-03-01 17:53:27 +0530

    Mysql installation, Database coectivity, Creating own model, django model form, storing data

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home-page'),
     path('about', views.about, name='about-page'),
     path('contact', views.contact, name='cont-page'),
+    path('add-std', views.add_student, name='add-std'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
+from . forms import StudentForm
 
 # Create your views here.
 def home(request):
@@ -10,3 +12,12 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def add_student(request):
+    if request.POST:
+       stdform=StudentForm(request.POST)
+       stdform.save()
+    else:
+        stdform=StudentForm()
+    context={'form':stdform}
+    return render(request, 'add_student.html', context)
